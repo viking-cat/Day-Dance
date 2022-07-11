f4f7828d3fb5a508101e867acefdc22a809674af
Patrik Kotiranta Lundberg <patrik.kotiranta.lundberg@gmail.com>
2022-07-11 14:13:29 +0200

    Extremely basic web server

--- server/main.go
+++ server/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"reflect"
 )
 
-type Config struct {
+type ServerConfig struct {
 	Name string `json:"serverName"`
 	Port uint   `json:"port"`
 }
@@ -17,21 +19,21 @@ func serveFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path)
 }
 
-func getJson(file string) bool {
+func getJson(file string) (*ServerConfig, error) {
 	log.Println("Reading " + file)
 
 	rawFile, fileError := ioutil.ReadFile(file)
 	if fileError != nil {
 		log.Println(fileError)
-		return false
+		return nil, fileError
 	}
 
-	var config Config
+	var config ServerConfig
 
 	marshalError := json.Unmarshal([]byte(rawFile), &config)
 	if marshalError != nil {
 		log.Println(marshalError)
-		return false
+		return nil, marshalError
 	}
 
 	log.Println("All: ", config)
@@ -39,47 +41,37 @@ func getJson(file string) bool {
 	log.Println("port type: ", reflect.TypeOf(config.Port))
 	log.Println("name type: ", config.Name)
 
-	/*
-			Fun experiments
-			Golang really need a better JSON Unmarshal
-			Maybe should write a library for fun
-		// var jsonResult map[string]interface{}
-		// https://stackoverflow.com/questions/28806951/accessing-nested-map-of-type-mapstringinterface-in-golang
-		// https://gist.github.com/ChristopherThorpe/fd3720efe2ba83c929bf4105719ee967
-		// https://irshadhasmat.medium.com/golang-simple-json-parsing-using-empty-interface-and-without-struct-in-go-language-e56d0e69968
-		marshalError := json.Unmarshal([]byte(rawFile), &jsonResult)
-		if marshalError != nil {
-			log.Println(marshalError)
-			return false
-		}
+	return &config, nil
+}
 
-		log.Println("port: ", jsonResult["port"])
-		log.Println("port type: ", reflect.TypeOf(jsonResult["port"]))
-		log.Println("name type: ", reflect.TypeOf(jsonResult["serverName"]))
-		log.Println("All of it\n", jsonResult)
-		log.Println("Final Experiment: ", jsonResult["something"])
-		//thatThing := jsonResult["something"]
-		//log.Println("Final Experiment: ", reflect.TypeOf(thatThing["abba"]))
-	*/
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got \"/\" request\n")
+	io.WriteString(w, "This is my website!\n")
+}
 
-	return true
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got \"/hello\" request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
 
 func main() {
 
-	// log.Println("Reading config.json")
-	// serverConfig, err := os.Open("config.json")
-	// if err != nil {
-	//log.Println(err)
-	//}
-
-	if getJson("settings.json") {
-		log.Println("success")
-	} else {
-		log.Println("failure")
+	// Get JSON config
+	config, jsonError := getJson("settings.json")
+	if jsonError != nil {
+		log.Println(jsonError)
 	}
 
-	// log.Printf("Starting Web Server")
-	// http.HandleFunc("/", serveFile)
-	// log.Fatal(http.ListenAndServe(":8081", nil))
+	// Define Routes
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	// Cast uint to string
+	port := ":" + fmt.Sprint(config.Port)
+
+	// Start server listener
+	serverError := http.ListenAndServe(port, nil)
+	if jsonError != serverError {
+		log.Println(serverError)
+	}
 }
